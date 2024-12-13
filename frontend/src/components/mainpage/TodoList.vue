8ddd948dbec0a2772380bfa21a0085091841867d
ChenYen-Yen <rickrickyen@gmail.com>
2024-12-14 03:31:28 +0800

    add a required marker in TodoList.vue

--- frontend/src/components/mainpage/TodoList.vue
+++ frontend/src/components/mainpage/TodoList.vue
@@ -6,8 +6,8 @@
     <table v-if="todoList.length">
         <thead>
             <tr>
-                <td>待辦事項</td>
-                <td>事項內容</td>
+                <td>待辦事項<span style="color: red;">*</span></td>
+                <td>事項內容<span style="color: red;">*</span></td>
                 <td>開始時間</td>
                 <td>結束時間</td>
                 <td v-if="isForPatch">已完成</td>
